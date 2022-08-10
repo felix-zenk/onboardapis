c7245fc57205235bf33e075e215736b996832c49
Felix <felix.zenk@web.de>
2022-08-11 01:35:20 +0200

    Initial commit

---
+++ onboardapis/__init__.py
@@ -0,0 +1,7 @@
+__description__ = 'A pure Python wrapper for the on-board APIs of many different transportation providers'
+__version__ = '1.0.0'
+__author__ = 'Felix Zenk'
+__email__ = 'felix.zenk@web.de'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 Felix Zenk'
+__url__ = 'https://github.com/felix-zenk/onboardapis'
