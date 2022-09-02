962f13f26031d9105df4da4b5f621333225891a7
Felix <felix.zenk@web.de>
2022-09-02 14:38:08 +0200

    Add conversions for coordinates

--- onboardapis/utils/conversions.py
+++ onboardapis/utils/conversions.py
@@ -2,7 +2,7 @@
 Convert between different units
 """
 
-from typing import Tuple
+from typing import Tuple, Union, Optional
 
 
 def coordinates_to_distance(start: Tuple[float, float], end: Tuple[float, float]) -> float:
@@ -22,7 +22,42 @@ def coordinates_to_distance(start: Tuple[float, float], end: Tuple[float, float]
     raise NotImplementedError()
 
 
+def coordinates_decimal_to_dms(coordinates: Tuple[float, float]) -> Tuple[Tuple[int, int, float], Tuple[int, int, float]]:
+    """
+    Convert the tuple ``coordinates`` of coordinates to degrees, minutes, seconds
+
+    :param coordinates: The decimal coordinates to convert to degrees, minutes, seconds
+    :type coordinates: Tuple[float, float]
+    :return: The coordinates in degrees, minutes, seconds
+    :rtype: Tuple[Tuple[int, int, float], Tuple[int, int, float]]
+    """
+    lat, lon = coordinates
+    lat_deg = int(abs(lat))
+    lat_min = int((abs(lat) - lat_deg) * 60)
+    lat_sec = ((abs(lat) - lat_deg) * 60 - lat_min) * 60
+    lon_deg = int(abs(lon))
+    lon_min = int((abs(lon) - lon_deg) * 60)
+    lon_sec = ((abs(lon) - lon_deg) * 60 - lon_min) * 60
+    return (-lat_deg if lat < 0 else lat_deg, lat_min, lat_sec), (-lon_deg if lon < 0 else lon_deg, lon_min, lon_sec)
+
+
+def coordinates_dms_to_decimal(coordinates: Tuple[Tuple[int, int, float], Tuple[int, int, float]]) -> Tuple[float, float]:
+    """
+    Convert the tuple ``coordinates`` of degrees, minutes, seconds to decimal degrees
+
+    :param coordinates: The degrees, minutes, seconds coordinates to convert to decimal degrees
+    :type coordinates: Tuple[Tuple[int, int, float], Tuple[int, int, float]]
+    :return: The coordinates in decimal degrees
+    :rtype: Tuple[float, float]
+    """
+    lat_deg, lat_min, lat_sec, lon_deg, lon_min, lon_sec = coordinates
+    lat = lat_deg + lat_min / 60 + lat_sec / 3600
+    lon = lon_deg + lon_min / 60 + lon_sec / 3600
+    return lat, lon
+
+
 # Convert between different units
+
 
 def ms_to_kmh(meters_per_second: float) -> float:
     """
