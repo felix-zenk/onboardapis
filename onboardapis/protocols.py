321e326684697d1e780fb165dc04ad7e5321658f
Felix <felix.zenk@web.de>
2023-10-08 17:45:16 +0200

    Add protocols for the mixins

---
+++ onboardapis/protocols.py
@@ -0,0 +1,33 @@
+from typing import Protocol
+
+from .types import ID, StationType
+
+
+class SupportsPosition(Protocol):
+    @property
+    def position(self) -> float: ...
+
+
+class SupportsSpeed(Protocol):
+    @property
+    def speed(self) -> float: ...
+
+
+class SupportsStations(Protocol):
+    @property
+    def stations_dict(self) -> dict[ID, StationType]: ...
+
+    @property
+    def stations(self) -> list[StationType]: ...
+
+    @property
+    def origin(self) -> StationType: ...
+
+    @property
+    def current_station(self) -> StationType: ...
+
+    @property
+    def destination(self) -> StationType: ...
+
+    @property
+    def delay(self) -> float: ...
