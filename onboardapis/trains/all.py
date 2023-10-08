370a5691c8da5fceda5c3c82c161f90043a6e818
Felix <felix.zenk@web.de>
2023-10-08 17:35:11 +0200

    Add onboardapis.trains.all for easy access

---
+++ onboardapis/trains/all.py
@@ -0,0 +1,9 @@
+from .austria.oebb import RailnetRegio
+from .germany.db import ICEPortal
+from .germany.flx import FlixTainment
+
+__all__ = [
+    "RailnetRegio",
+    "ICEPortal",
+    "FlixTainment",
+]
