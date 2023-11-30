65e45cf6b2172d87d145e467d954746366e8c687
Felix <felix.zenk@web.de>
2023-11-30 17:36:33 +0100

    Remove all module

--- onboardapis/train/all.py
+++
@@ -1,9 +0,0 @@
-from .austria.oebb import RailnetRegio
-from .germany.db import ICEPortal
-from .germany.flx import FlixTainment
-
-__all__ = [
-    "RailnetRegio",
-    "ICEPortal",
-    "FlixTainment",
-]
