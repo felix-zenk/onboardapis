0e45caaa92f84501ae376cf2870fd778030fe298
Felix <felix.zenk@web.de>
2023-12-07 16:57:59 +0100

    Add metronom to bth

---
+++ onboardapis/train/de/bth/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package contains APIs from trains of the ALSTOM Transportation Germany GmbH.
+"""
+from ..me import CaptivePortal as MetronomCaptivePortal
+
+__all__ = [
+    'MetronomCaptivePortal',
+]
