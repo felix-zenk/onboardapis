86a42f2ffde55fd05a5bd36baf6d5aff63916623
Felix <felix.zenk@web.de>
2024-01-13 13:55:18 +0100

    Fix RailnetRegio downgraded

--- onboardapis/train/at/obb/apis.py
+++ onboardapis/train/at/obb/apis.py
@@ -1,163 +1,29 @@
 from __future__ import annotations
 
-from datetime import datetime, timedelta
-
-from ...._types import ID
-from ....exceptions import DataInvalidError
 from ....units import ms
-from ....data import (
-    default,
-    ScheduledEvent,
-    Position,
-)
-from ... import Train, ConnectingTrain, TrainStation
-from .connectors import RailnetConnector
+from ....data import Position
+from ... import Train, IncompleteTrainMixin
+from .connectors import RailnetRegioConnector
 
 
-class RailnetRegio(Train):
+class RailnetRegio(IncompleteTrainMixin, Train):
     """
     Wrapper for interacting with the ÖBB RailnetRegio API
     """
 
-    _data: RailnetConnector()
+    _data: RailnetRegioConnector
 
     def __init__(self):
-        self._data = RailnetConnector()
+        self._data = RailnetRegioConnector()
         super().__init__()
-
-    def now(self) -> datetime:
-        return datetime.fromtimestamp(int(default(
-            self._data["combined"].get("operationalMessagesInfo", {}).get("time"), 0
-        )))
-
-    @property
-    def type(self) -> str:
-        return self._data["train_info"].get("trainType", None)
-
-    @property
-    def number(self) -> str:
-        return self._data["train_info"].get("lineNumber", None)
-
-    @property
-    def id(self) -> ID:
-        # Not available in Railnet
-        return self.number
-
-    @property
-    def stations_dict(self) -> dict[ID, TrainStation]:
-        def to_datetime(data: str, __future: bool = None) -> datetime | None:
-            if default(data, None) is None:
-                return None
-            # Now
-            now = self.now()
-            # Assume that the day is the same as the current day
-            obj = datetime.strptime(data, "%H:%M").replace(
-                year=now.year, month=now.month, day=now.day
-            )
-
-            if __future is not None:
-                # We know that the event is in the future,
-                # but obj is currently in the past
-                if __future and obj < now:
-                    obj += timedelta(days=1)
-                # We know that the event is in the past,
-                # but obj is currently in the future
-                elif not __future and obj > now:
-                    obj -= timedelta(days=1)
-            return obj
-
-        stations = {}
-        distance = 0.0
-        future = False
-        for station_json in self._data["combined"].get("stationList", []):
-            # The first station has no distance
-            if station_json.get("distance", "") != "":
-                distance += float(default(station_json.get("distance"), __default=0.0))
-            connections = list(
-                [
-                    ConnectingTrain(
-                        vehicle_type=connection.get("type", None),
-                        line_number=connection.get("line", None),
-                        platform=ScheduledEvent(
-                            scheduled=connection.get("track", {}).get("all", None),
-                            actual=connection.get("track", {}).get("all", None),
-                        ),
-                        destination=connection.get("destination", {}).get("all", None),
-                        departure=ScheduledEvent(
-                            scheduled=to_datetime(
-                                connection.get("schedule", None), True
-                            ),
-                            actual=to_datetime(connection.get("actual", None), True),
-                        ),
-                    )
-                    for connection in self._data["combined"].get("connectionInfo", {}).get("connections", [])
-                    if self._data["combined"].get("connectionInfo", {}).get("station_id", -1) == station_json.get("id")
-                ]
-            )
-
-            stations[station_json.get("id")] = TrainStation(
-                station_id=station_json.get("id"),
-                name=station_json.get("name", {}).get("all", None),
-                platform=ScheduledEvent(
-                    station_json.get("track", {}).get("all", None),
-                    station_json.get("track", {}).get("all", None),
-                ),
-                arrival=ScheduledEvent(
-                    to_datetime(
-                        station_json.get("arrivalSchedule", None),
-                        self.delay > timedelta() if future else False,
-                    ),
-                    to_datetime(station_json.get("arrivalForecast", None), future),
-                ),
-                departure=ScheduledEvent(
-                    to_datetime(
-                        station_json.get("departureSchedule", None),
-                        self.delay > timedelta() if future else False,
-                    ),
-                    to_datetime(station_json.get("departureForecast", None), future),
-                ),
-                distance=distance,
-                connections=connections,
-            )
-
-            future = future or station_json.get("id", -1) == self._data["combined"].get("currentStation", {}).get("id")
-        return stations
-
-    @property
-    def current_station(self) -> TrainStation:
-        # Get the current station id
-        station_id = (self._data["combined"].get("currentStation", {}).get("id", -1))
-        # Get the station from the stations dict
-        try:
-            return self.stations_dict[station_id]
-        except AttributeError as e:
-            raise DataInvalidError("No current station found") from e
 
     @property
     def speed(self) -> float:
-        return ms(kmh=float(
-            self._data["combined"].get("operationalMessagesInfo", {}).get("speed", 0)
-        ))
-
-    @property
-    def distance(self) -> float:
-        try:
-            reference_station = self._data["combined"].get("operationalMessagesInfo", {}).get("distanceStation", -1)
-            return (
-                self.stations_dict[reference_station].distance
-                + float(self._data["combined"].get("operationalMessagesInfo", {}).get("distance", 0))
-            )
-        except AttributeError as e:
-            raise DataInvalidError("No reference station found for distance") from e
+        return ms(kmh=self._data['gps']['JSON']['speed'])
 
     @property
     def position(self) -> Position:
-        map_info = self._data["combined"].get("mapInfo", {})
         return Position(
-            latitude=(float(map_info.get("latitude")) if default(map_info.get("latitude")) is not None else None),
-            longitude=(float(map_info.get("longitude")) if default(map_info.get("longitude")) is not None else None),
+            latitude=self._data['gps']['JSON']['lat'],
+            longitude=self._data['gps']['JSON']['lon'],
         )
-
-    @property
-    def delay(self) -> timedelta:
-        return timedelta(seconds=float(self._data["combined"].get("operationalMessagesInfo", {}).get("delay", 0)))
--- onboardapis/train/at/obb/connectors.py
+++ onboardapis/train/at/obb/connectors.py
@@ -1,23 +1,43 @@
 import time
 from functools import lru_cache
+from typing import TypedDict
+
+from typing_extensions import deprecated
+
 from ....data import (
     RESTDataConnector,
     store,
 )
 
+Endpoints = TypedDict("Endpoints", {
+    'train_info': int,
+    'gps': dict,
+    'speed': float,
+})
 
-class RailnetConnector(RESTDataConnector):
+
+class RailnetRegioConnector(RESTDataConnector):
     API_URL = "https://railnet.oebb.at"
 
     @store('train_info')
     @lru_cache
-    def train_info(self) -> dict:
-        return self.get("api/trainInfo").json()
+    def train_info(self) -> int:
+        return int(self.get("api/trainInfo").text)
 
+    @store('gps')
+    def gps(self) -> dict:
+        return self.get("api/gps").json()
+
+    @store('speed')
+    def speed(self) -> float:
+        return float(self.get("api/speed").text)
+
+    @deprecated('combined has been removed from the API')
     @store('combined')
     def combined(self) -> dict:
-        return self.get("/assets/modules/fis/combined.json", params={"_time": time.time()}).json()
+        return self.get("assets/modules/fis/combined.json", params={"_time": time.time()}).json()
 
     def refresh(self) -> None:
         self.train_info()
-        self.combined()
+        self.gps()
+        self.speed()
