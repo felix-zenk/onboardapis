a03cc71f421ba78b6ce69e675233e7c5ffc5390f
Felix <felix.zenk@web.de>
2024-02-26 22:53:57 +0100

    Switch to MetronomInternetAccessInterface

--- onboardapis/train/de/me/apis.py
+++ onboardapis/train/de/me/apis.py
@@ -1,32 +1,15 @@
-from ... import IncompleteTrainMixin, Train
+from ....mixins import InternetAccessMixin
+from ... import Train
+from .connectors import MetronomCaptivePortalConnector, MetronomInternetAccessInterface
 
-from .connectors import MetronomCaptivePortalConnector
 
-
-class MetronomCaptivePortal(IncompleteTrainMixin, Train):
+class MetronomCaptivePortal(InternetAccessMixin, Train):
     """
     Basically no information about the train.
     """
     _data: MetronomCaptivePortalConnector
+    _internet_access: MetronomInternetAccessInterface
 
     def __init__(self) -> None:
         self._data = MetronomCaptivePortalConnector()
-
-    @property
-    def logged_in(self) -> bool:
-        """
-        Whether the client is logged in to the captive portal.
-        """
-        return self._data['captive_portal']['has_internet_access']
-
-    def do_login(self) -> None:
-        """
-        Log in to the captive portal.
-        """
-        self._data.login()
-
-    def do_logout(self) -> None:
-        """
-        Log out from the captive portal.
-        """
-        self._data.logout()
+        self._internet_access = MetronomInternetAccessInterface()
