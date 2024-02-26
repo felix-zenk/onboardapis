a6f52ee9ed0cfc014e930559cab0617aebfd9338
Felix <felix.zenk@web.de>
2024-02-26 22:48:31 +0100

    Add GeoDataMixin, InternetAccessMixin

--- onboardapis/mixins.py
+++ onboardapis/mixins.py
@@ -10,12 +10,9 @@ from datetime import timedelta
 from typing import Generic
 
 from .exceptions import DataInvalidError
-from .data import Position
+from .data import Position, InternetAccessInterface
 from ._types import ID, StationType
-
-
-class StatisticsMixin(metaclass=ABCMeta):  # TODO wifi metrics, statistics
-    pass
+from . import Station
 
 
 class PositionMixin(metaclass=ABCMeta):
@@ -32,47 +29,76 @@ class PositionMixin(metaclass=ABCMeta):
 
 
 class SpeedMixin(metaclass=ABCMeta):
+    """
+    Functionality for a vehicle that provides information about its speed.
+    """
+
     @property
     @abstractmethod
     def speed(self) -> float:
-        """
-        Get the current speed of the vehicle.
+        """The current speed of the vehicle.
 
-        :return: The current speed of the vehicle
-        :rtype: float
+        Returns:
+            The current speed in \\frac{meters}{second}
         """
         raise NotImplementedError
 
 
 class StationsMixin(Generic[StationType], metaclass=ABCMeta):
+    """
+    Functionality for a vehicle that provides information on the journey.
+    """
+
+    def calculate_distance(self, station: Station) -> float:
+        """Calculate the distance in meters between the train and a station.
+
+        Use the trains `position` or `distance` to calculate the distance to `station`.
+
+        Args:
+            station: The station to calculate the distance to
+
+        Returns:
+            The distance between the train and the station in meters
+        """
+        if hasattr(self, 'position'):
+            position: Position = getattr(self, 'position')
+            return station.calculate_distance(position)
+
+        if hasattr(self, 'distance'):
+            distance: float = getattr(self, 'distance')
+            return station.calculate_distance(distance)
+
+        raise NotImplementedError
+
     @property
     @abstractmethod
     def stations_dict(self) -> dict[ID, StationType]:
-        """
-        The stations that this vehicle passes through returned as a dict with the station ID as the key.
-        Mostly ID will be of type str.
+        """A dict containing the stations on the journey.
 
-        :return: The stations as a dict with
-        :rtype: Dict[str, StationType]
+        The stations that this vehicle passes through returned as a dict with the station ID as the key.
+
+        Returns:
+            The stations as a dict
         """
-        pass
+        raise NotImplementedError
 
     @property
     def stations(self) -> list[StationType]:
-        """
-        Return the stations as a list
+        """Return the stations as a list.
 
-        :rtype: List[StationType]
+        Return a list that contains every station from `onboardapis.mixins.StationsMixin.stations_dict`.
+
+        Returns:
+            The `stations_dict` values
         """
         return list(self.stations_dict.values())
 
     @property
     def origin(self) -> StationType:
-        """
-        The station where this vehicle started the current journey
+        """The station where this vehicle started the current journey.
 
-        :return: The first station on this trip
-        :rtype: StationType
+        Returns:
+            The first station on this trip.
         """
         if len(self.stations) > 0:
             return self.stations[0]
@@ -81,23 +107,19 @@ class StationsMixin(Generic[StationType], metaclass=ABCMeta):
     @property
     @abstractmethod
     def current_station(self) -> StationType:
-        """
-        The station where this vehicle will arrive next or is currently at
+        """The next station.
 
-        :return: The current station
-        :rtype: StationType
+        Returns:
+            The station where this vehicle will arrive next or is currently at.
         """
-        # Get the current station id
-        # Get the station from the stations dict
-        pass
+        raise NotImplementedError
 
     @property
     def destination(self) -> StationType:
-        """
-        The station where this vehicle will end the current journey
+        """The station where this vehicle terminates the current journey.
 
-        :return: The last station on this trip
-        :rtype: StationType
+        Returns:
+            The destination station.
         """
         if len(self.stations) > 0:
             return self.stations[-1]
@@ -105,12 +127,27 @@ class StationsMixin(Generic[StationType], metaclass=ABCMeta):
 
     @property
     def delay(self) -> timedelta:
-        """
-        The current delay of the vehicle
+        """The current delay.
 
-        :return: The delay of the vehicle
-        :rtype: timedelta
+        The current delay calculated by the `current_station`
+        scheduled arrival time vs. actual arrival time.
+
+        Returns:
+            The current delay of the vehicle as a `datetime.timedelta` object.
         """
         return timedelta(seconds=(
             self.current_station.arrival.actual - self.current_station.arrival.scheduled
         ).total_seconds())
+
+
+class GeoDataMixin(PositionMixin, StationsMixin[StationType], metaclass=ABCMeta):
+    pass
+
+
+class InternetAccessMixin(metaclass=ABCMeta):
+    _internet_access: InternetAccessInterface
+
+    @property
+    def internet_access(self) -> InternetAccessInterface:
+        """An interface to enable or disable the internet access for this device."""
+        return self._internet_access
