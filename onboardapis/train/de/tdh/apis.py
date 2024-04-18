ce95d2bddcd29427350e9b7a7311982979d79c22
Felix Zenk <felix.zenk@web.de>
2024-04-18 19:46:20 +0200

    Fix not initialized

--- onboardapis/train/de/tdh/apis.py
+++ onboardapis/train/de/tdh/apis.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 
-from ...third_party.unwired import GenericUnwiredTrain, GenericUnwiredAPI
+from ...third_party.unwired import GenericUnwiredTrain, GenericUnwiredAPI, GenericUnwiredInternetAccessInterface
 from ...third_party.unwired.mixins import UnwiredJourneyMixin
 
 logger = logging.getLogger(__name__)
@@ -11,7 +11,8 @@ logger = logging.getLogger(__name__)
 class SBahnHannover(GenericUnwiredTrain, UnwiredJourneyMixin):
     """Implementation for the S-Bahn Hannover."""
 
-    def init(self) -> None:
+    def __init__(self) -> None:
         self._api = GenericUnwiredAPI()
+        self._internet_access = GenericUnwiredInternetAccessInterface()
         GenericUnwiredTrain.__init__(self)
         UnwiredJourneyMixin.__init__(self)
