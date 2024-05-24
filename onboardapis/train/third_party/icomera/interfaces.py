263a79c05c07781e60fc195324f227faa22525ac
Felix Zenk <felix.zenk@web.de>
2024-05-24 12:22:56 +0200

    Update GenericIcomeraInternetAccessInterface

--- onboardapis/train/third_party/icomera/interfaces.py
+++ onboardapis/train/third_party/icomera/interfaces.py
@@ -26,14 +26,12 @@ class GenericIcomeraInternetAccessInterface(InternetAccessInterface):
 
     def enable(self):
         response = self._api.get('de')
-        soup = BeautifulSoup(response.text, 'html.parser')
-        if soup.find(class_='user-offline') is None:
+        if BeautifulSoup(response.text, 'html.parser').find(class_='user-offline') is None:
             return  # Already online
 
-        csrf_token = response.cookies['csrf']
         response = self._api.post('de/', data={
             'login': True,
-            'CSRFToken': csrf_token,
+            'CSRFToken': response.cookies['csrf'],
         })
         response.raise_for_status()
         if BeautifulSoup(response.text, 'html.parser').find(class_='user-online') is None:
@@ -41,14 +39,12 @@ class GenericIcomeraInternetAccessInterface(InternetAccessInterface):
 
     def disable(self):
         response = self._api.get('de')
-        soup = BeautifulSoup(response.text, 'html.parser')
-        if soup.find(class_='user-online') is None:
+        if BeautifulSoup(response.text, 'html.parser').find(class_='user-online') is None:
             return  # Already offline
 
-        csrf_token = response.cookies['csrf']
         response = self._api.post('de/', data={
             'logout': True,
-            'CSRFToken': csrf_token,
+            'CSRFToken': response.cookies['csrf'],
         })
         response.raise_for_status()
         if BeautifulSoup(response.text, 'html.parser').find(class_='user-offline') is None:
