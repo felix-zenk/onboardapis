179136d686d3eb06f27c30df751603ccbc42915e
felix-zenk <felix.zenk@web.de>
2024-08-25 16:09:03 +0200

    Raise appropriate error

--- onboardapis/train/third_party/unwired/apis.py
+++ onboardapis/train/third_party/unwired/apis.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import logging
 import re
 
+from ....exceptions import DataInvalidError
 from ....mixins import InternetAccessMixin
-from ....data import ID
 from ... import Train
 from .interfaces import GenericUnwiredAPI, GenericUnwiredInternetAccessInterface
 
@@ -24,13 +24,19 @@ class GenericUnwiredTrain(Train, InternetAccessMixin):
         InternetAccessMixin.__init__(self)
 
     @property
-    def id(self) -> ID:
+    def id(self) -> str:
+        if 'id' not in self._api['journey']:
+            raise DataInvalidError('The trains ID could not be fetched from the server!')
         return self._api['journey']['id']
 
     @property
     def type(self) -> str:
+        if 'line' not in self._api['journey']:
+            raise DataInvalidError('The train type could not be fetched from the server!')
         return re.match(r'(\w+)\d+', self._api['journey']['line']).group(1)
 
     @property
     def line_number(self) -> str:
+        if 'line' not in self._api['journey']:
+            raise DataInvalidError('The line number could not be fetched from the server!')
         return re.match(r'\w+(\d+)', self._api['journey']['line']).group(1)
