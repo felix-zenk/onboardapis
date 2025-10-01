f183c3c07deb2a8cd4cdba1608c3058898fb45ee
felix-zenk <felix.zenk@web.de>
2025-10-01 21:58:09 +0200

    Add Airport class

--- onboardapis/plane/__init__.py
+++ onboardapis/plane/__init__.py
@@ -4,16 +4,21 @@ Package for planes
 ## Operator ID
 
 The operator ID for planes is the
-[3 letter ICAO airline designator](https://en.wikipedia.org/wiki/List_of_airline_codes#Codes).
+[3-letter ICAO airline designator](https://en.wikipedia.org/wiki/List_of_airline_codes#Codes).
+
+---
 """
 from __future__ import annotations
 
 from abc import ABCMeta
+from dataclasses import dataclass
 
-from .. import Vehicle
+from .. import Vehicle, Station
+from ..data import ScheduledEvent
 
 __all__ = [
     "Plane",
+    "Airport",
 ]
 
 
@@ -21,3 +26,10 @@ class Plane(Vehicle, metaclass=ABCMeta):
     """
     Base class for planes
     """
+
+
+@dataclass
+class Airport(Station):
+    """An `onboardapis.Station` with the additional information of a gate."""
+    gate: ScheduledEvent[str] | None
+    """The gate where the plane is parked at."""
