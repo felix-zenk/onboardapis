62c039057154f665677e5156e0614705a0ebe991
felix-zenk <felix.zenk@web.de>
2025-10-03 14:18:18 +0200

    Add plane.third_party.inflight_dublin

---
+++ onboardapis/plane/third_party/inflight_dublin/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import logging
+
+from .apis import Everhub
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    'Everhub',
+]
---
+++ onboardapis/plane/third_party/inflight_dublin/apis.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from ..betria_interactive import FlightPath3DPortal, FlightPath3DAPI
+
+
+class Everhub(FlightPath3DPortal):
+    _api: FlightPath3DAPI
